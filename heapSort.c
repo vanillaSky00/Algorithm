8bb38fa31f8a7079314aada6a97374f359c3c0a9
vanillaSky00 <pipichun2@gmail.com>
2025-04-03 17:53:39 +0800

    [HeapSort]

--- heapSort.c
+++ heapSort.c
@@ -1,73 +1,52 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-//need to revise, no need to add a new struct heap
-typedef struct heap{
-    int* heap_h;
-    int capacity;
-    int heap_size;
-} heap;
-
-heap* heapSort(int a[], int len);
-heap* build_maxHeap(int a[], int size);
-void max_heapify(heap* h, int i);
-void swap(int* a, int* b);
-
+void heap_sort(int a[], int n);
+void max_heapify(int a[], int n, int i);
+void swap(int* x, int* y);
 
 int main(){
     int nums1[]= {3, 2, 44, 0, 4, 12, 6, 3, -6};
-    int len = sizeof(nums1)/sizeof(nums1[0]);
-    heap* h = heapSort(nums1, len);
-
+    int len = sizeof(nums1)/ sizeof(nums1[0]);
+    heap_sort(nums1, len);
     for(int i=0; i<len; i++){
-        printf("%d,", h->heap_h[i]);
+        printf("%d,", nums1[i]);
     }
-    return 0;
 }
 
-
-heap* heapSort(int a[], int len){
-    heap* h = build_maxHeap(a, len);
-    for(int i=len-1; i>0; i--){
-        swap(&h->heap_h[0], &h->heap_h[i]);
-        h->heap_size--;
-        max_heapify(h, 0);
+void heap_sort(int a[], int n){
+    //get a max heap first, consider n/2 - 1, not n, more quicker
+    for(int i= n/2 - 1; i>=0; i--){
+        max_heapify(a, n, i); 
     }
-    return h;
+
+    //swap the max and adjust to max heap again
+    for(int j=n-1; j>0; j--){
+        swap(&a[0], &a[j]);
+        max_heapify(a, j, 0);
+    }
 }
 
-heap* build_maxHeap(int a[], int size){
-    heap* h = (heap*)malloc(sizeof(heap));
-    h->heap_size = size;
-    h->heap_h = (int*)malloc(sizeof(int) * h->heap_size);
-    for(int i=0; i<h->heap_size; i++){
-        h->heap_h[i] = a[i];
-    }
-    for(int i=h->heap_size/2 - 1; i>=0; i--){
-        max_heapify(h, i);
-    }
-    return h;
-}
-
-void max_heapify(heap* h, int i){
+// use bubble down method, in contrast to my_heapsort
+void max_heapify(int a[], int n, int i){
     int largest = i;
-    int left = 2*i + 1;
-    int right = 2*i + 2;
-    if(left < h->heap_size && h->heap_h[left] > h->heap_h[largest]){
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    if(left < n && a[left] > a[largest]){
         largest = left;
     }
-    if(right < h->heap_size && h->heap_h[right] > h->heap_h[largest]){
+
+    if(right < n && a[right] > a[largest]){
         largest = right;
     }
 
     if(largest != i){
-        swap(&(h->heap_h[largest]), &(h->heap_h[i]));
-        max_heapify(h, largest);
+        swap(&a[largest], &a[i]);
+        max_heapify(a, n, largest);
     }
 }
 
-void swap(int* a, int* b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(int* x, int* y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
---
+++ heapSort_slower.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include"heapSort.c"
+void my_heap_sort(int a[], int n);
+void my_swap(int* a, int* b);
+int comp_int(const void* x1, const void* x2);
+
+int main(){
+    int LEN = 10000;
+    int nums1[LEN];
+    int nums2[LEN];
+    srand(time(NULL));
+    for(int i = 0; i<10; i++){
+        for(int j=0; j<LEN; j++){
+            nums1[j] = rand() % 100;
+            nums2[j] = nums1[j];
+        }
+        
+        my_heap_sort(nums1, LEN);
+        heap_sort(nums2, LEN);
+        //qsort(nums2, LEN, sizeof(int), comp_int);
+        for(int i=0; i<LEN; i++){
+            if(nums1[i] != nums2[i]){
+                printf("%d%s", nums1[i], (i != LEN - 1) ? ", ": "\n");
+            }
+        }
+        printf("done");
+    }
+    return 0;
+}
+
+// bubble up method, and without making a max heap first
+// gpt says it is not a heapsort??
+void my_heap_sort(int a[], int n){
+    //build heap: just use given array
+    int i, j, p;
+    //find the max and swap
+    for(i=n-1; i>=0; i--){
+        for(j=i; j>=0; j--){
+            p = (j-1)/2;
+            if(p>=0 && (a[j] > a[p])){
+                my_swap(&a[j], &a[p]);
+            }
+        }
+        my_swap(&a[0], &a[i]);
+    }
+}
+
+void my_swap(int* a, int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int comp_int(const void* x1, const void* x2){
+    return (*(int*)x1) - (*(int*)x2);
+}
