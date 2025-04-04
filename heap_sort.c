8d917f130fe646059dea5ee782fd52f57891a139
vanillaSky00 <pipichun2@gmail.com>
2025-04-04 15:37:50 +0800

    [unit test]

--- heap_sort.c
+++ heap_sort.c
@@ -1,17 +1,8 @@
 #include<stdio.h>
 
 void heap_sort(int a[], int n);
-void max_heapify(int a[], int n, int i);
-void swap(int* x, int* y);
-
-int main(){
-    int nums1[]= {3, 2, 44, 0, 4, 12, 6, 3, -6};
-    int len = sizeof(nums1)/ sizeof(nums1[0]);
-    heap_sort(nums1, len);
-    for(int i=0; i<len; i++){
-        printf("%d,", nums1[i]);
-    }
-}
+static void max_heapify(int a[], int n, int i);
+static void swapp(int* x, int* y);
 
 void heap_sort(int a[], int n){
     //get a max heap first, consider n/2 - 1, not n, more quicker
@@ -21,13 +12,13 @@ void heap_sort(int a[], int n){
 
     //swap the max and adjust to max heap again
     for(int j=n-1; j>0; j--){
-        swap(&a[0], &a[j]);
+        swapp(&a[0], &a[j]);
         max_heapify(a, j, 0);
     }
 }
 
 // use bubble down method, in contrast to my_heapsort
-void max_heapify(int a[], int n, int i){
+static void max_heapify(int a[], int n, int i){
     int largest = i;
     int left = 2 * i + 1;
     int right = 2 * i + 2;
@@ -40,13 +31,22 @@ void max_heapify(int a[], int n, int i){
     }
 
     if(largest != i){
-        swap(&a[largest], &a[i]);
+        swapp(&a[largest], &a[i]);
         max_heapify(a, n, largest);
     }
 }
 
-void swap(int* x, int* y){
+static void swapp(int* x, int* y){
     int temp = *x;
     *x = *y;
     *y = temp;
 }
+
+// int main(){
+//     int nums1[]= {3, 2, 44, 0, 4, 12, 6, 3, -6};
+//     int len = sizeof(nums1)/ sizeof(nums1[0]);
+//     heap_sort(nums1, len);
+//     for(int i=0; i<len; i++){
+//         printf("%d,", nums1[i]);
+//     }
+// }
